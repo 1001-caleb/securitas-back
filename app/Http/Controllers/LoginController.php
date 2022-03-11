82fbfc577af25cecfce5a9d97694c3e9ab43a854
1001-caleb <dmoises084@gmail.com>
2022-03-11 16:47:37 -0500

    commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->only("email", "password");
+
+        if(!Auth::attempt($credentials)){
+            return response ([
+                "message" => "Usuario y/o contraseña invalido."
+            ], 401);
+        }
+
+        $accessToken = Auth::user()->createToken("authTestToken")->accessToken;
+
+        return response ([
+            "user" => Auth::user(),
+            "access_token" => $accessToken
+        ]);
+    }
+}
